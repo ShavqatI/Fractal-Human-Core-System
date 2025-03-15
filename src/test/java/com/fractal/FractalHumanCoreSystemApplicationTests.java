528b5d3475560fc87da7b7693a26ddb8c4dba3c7
Shavqat <shavqatibrohimov@gmail.com>
2025-03-15 20:17:42 +0500

    Initial commit

---
+++ src/test/java/com/fractal/FractalHumanCoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fractal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractalHumanCoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
