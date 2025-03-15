a2258e067039f860aff8eda4c6e5f45ea44aa6d0
Shavqat <shavqatibrohimov@gmail.com>
2025-03-15 21:52:15 +0500

    Domain object Employee changes

--- src/main/java/com/fractal/domain/employee/Employee.java
+++ src/main/java/com/fractal/domain/employee/Employee.java
@@ -1,12 +1,17 @@
 package com.fractal.domain.employee;
 
 import com.fractal.domain.abstraction.Person;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "employee", schema = "employee_schema", catalog = "fractal")
@@ -15,6 +20,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee extends Person {
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EmploymentHistory> employmentHistories  = new ArrayList<>();
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BusinessTrip> businessTrips  = new ArrayList<>();
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vacation> vacations  = new ArrayList<>();
 
 
 }
