2532f0bcd06fda31ce3afecf248de626c3771c22
Shavqat <shavqatibrohimov@gmail.com>
2025-04-06 20:04:54 +0500

    big changes and exceptions

--- src/main/java/com/fractal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fractal/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.fractal.config;
+package com.fractal.exception;
 
-import com.fractal.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
