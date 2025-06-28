2154e59640c5b21837432a73e580a1b5f0c6498a
Shavqat <shavqatibrohimov@gmail.com>
2025-06-28 15:30:30 +0500

    Controllers education  has been changed

--- src/main/java/com/fractal/controller/employee_management/employee/education/EducationDocumentTypeController.java
+++ src/main/java/com/fractal/controller/employee_management/employee/education/EducationDocumentTypeController.java
@@ -1,4 +1,4 @@
-package com.fractal.controller.employee_management;
+package com.fractal.controller.employee_management.employee.education;
 
 
 import com.fractal.domain.employee_management.education.document_type.EducationDocumentTypeService;
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/employee-management/education-document-type")
+@RequestMapping("/api/v1/employee-management/education/document-type")
 @RequiredArgsConstructor
 public class EducationDocumentTypeController {
 
