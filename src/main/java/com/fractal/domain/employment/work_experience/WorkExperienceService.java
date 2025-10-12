20a7aa79a2ab7a11c30acce5c764a5e92cccbff3
Shavqat <shavqatibrohimov@gmail.com>
2025-10-12 17:00:25 +0500

    Vacation module  has been added

--- src/main/java/com/fractal/domain/employment/work_experience/WorkExperienceService.java
+++
@@ -1,6 +0,0 @@
-package com.fractal.domain.employment.work_experience;
-
-public interface WorkExperienceService {
-    WorkExperience getById(Long id);
-    WorkExperience save(WorkExperience workExperience);
-}
--- src/main/java/com/fractal/domain/employment/work_experience/separation_reason/dto/SeparationReasonCompactResponse.java
+++
@@ -1,4 +0,0 @@
-package com.fractal.domain.employment.work_experience.separation_reason.dto;
-
-public record SeparationReasonCompactResponse(Long id, String code, String name) {
-}
