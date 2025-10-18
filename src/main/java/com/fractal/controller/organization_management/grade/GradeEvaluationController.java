2c41b7ed1d0a17b234644a80054c352c2b002863
Shavqat <shavqatibrohimov@gmail.com>
2025-10-18 13:17:47 +0500

    GradeEvaluation module  has been added

---
+++ src/main/java/com/fractal/controller/organization_management/grade/GradeEvaluationController.java
@@ -0,0 +1,45 @@
+package com.fractal.controller.organization_management.grade;
+
+
+import com.fractal.domain.organization_management.grade.evaluation.GradeEvaluationService;
+import com.fractal.domain.organization_management.grade.method.dto.GradingEvaluationRequest;
+import com.fractal.domain.organization_management.grade.method.dto.GradingEvaluationResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/organization-management/grade/{gradeId}/evaluation")
+@RequiredArgsConstructor
+public class GradeEvaluationController {
+
+    private final GradeEvaluationService evaluationService;
+
+    @PostMapping()
+    public ResponseEntity<GradingEvaluationResponse> create(@PathVariable Long gradeId, @RequestBody @Valid GradingEvaluationRequest dto) {
+        return new ResponseEntity<>(evaluationService.toDTO(evaluationService.create(gradeId,dto)), HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<GradingEvaluationResponse>> getAll(@PathVariable Long gradeId) {
+        return ResponseEntity.ok(evaluationService.getAllByGradeId(gradeId).stream().map(evaluationService::toDTO).collect(Collectors.toList()));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<GradingEvaluationResponse> getById(@PathVariable Long gradeId,@PathVariable Long id) {
+        return ResponseEntity.ok(evaluationService.toDTO(evaluationService.getById(gradeId,id)));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<GradingEvaluationResponse> update(@PathVariable Long gradeId, @PathVariable Long id, @RequestBody @Valid GradingEvaluationRequest dto) {
+        return ResponseEntity.ok(evaluationService.toDTO(evaluationService.update(gradeId,id, dto)));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long employeeId, @PathVariable Long id) {
+        evaluationService.delete(employeeId,id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
