fba33767136a985dd53ea651fea2c6d942c5be30
Shavqat <shavqatibrohimov@gmail.com>
2025-10-24 18:00:11 +0500

    new changes module  has been added

--- src/main/java/com/fractal/domain/organization_management/department/dto/DepartmentCompactResponse.java
+++ src/main/java/com/fractal/domain/organization_management/department/dto/DepartmentCompactResponse.java
@@ -1,4 +1,4 @@
 package com.fractal.domain.organization_management.department.dto;
 
-public record DepartmentCompactResponse(Long id, String code, String name) {
+public record DepartmentCompactResponse(Long id, String name) {
 }
