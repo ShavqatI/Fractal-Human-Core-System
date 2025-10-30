e74ab6737c9e5029915fb56f222b6d9b000d6ecf
Shavqat <shavqatibrohimov@gmail.com>
2025-10-30 18:07:21 +0500

    new changes module  has been added

--- src/main/java/com/fractal/domain/employee_management/business_trip/location/address/dto/BusinessTripLocationAddressRequest.java
+++ src/main/java/com/fractal/domain/employee_management/business_trip/location/address/dto/BusinessTripLocationAddressRequest.java
@@ -1,4 +1,17 @@
 package com.fractal.domain.employee_management.business_trip.location.address.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,       // Use a type name in JSON
+        property = "locationType",      // JSON must include this field
+        visible = true
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ExternalBusinessTripLocationAddressRequest.class, name = "EXTERNAL"),
+        @JsonSubTypes.Type(value = InternalBusinessTripLocationAddressRequest.class, name = "INTERNAL")
+})
 public interface BusinessTripLocationAddressRequest {
 }
